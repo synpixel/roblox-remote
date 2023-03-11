ccacb98dfac243ca6a5e1411e098379f6c7efb62
DogeyStudio <syntaxwastaken2@gmail.com>
2023-03-12 00:58:27 +0100

    Implement Discord's interactions url endpoint feature

--- api/interactions.ts
+++ api/interactions.ts
@@ -2,6 +2,12 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
 import type { Readable } from 'node:stream';
 
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+};
+
 async function buffer(readable: Readable) {
     const chunks = [];
     for await (const chunk of readable) {
@@ -17,8 +23,10 @@ export default async function interactions(
     const message = request.body;
     const buf = await buffer(request);
     
-    const signature = request.headers["X-Signature-Ed25519"];
-    const timestamp = request.headers["X-Signature-Timestamp"];
+    const signature = request.headers["x-signature-ed25519"];
+    const timestamp = request.headers["x-signature-timestamp"];
+
+    console.log(request);
 
     const isValidRequest = verifyKey(buf.toString("utf8"), signature as string, timestamp as string, process.env.PUBLIC_KEY as string);
 
@@ -26,14 +34,20 @@ export default async function interactions(
         return response.status(401).end("Bad request signature");
     }
 
+    if (message.type === InteractionType.PING) {
+        return response.status(200).send({
+            type: InteractionResponseType.PONG,
+        })
+    }
+
     if (message.type === InteractionType.APPLICATION_COMMAND) {
-        response.status(200).send({
+        return response.status(200).send({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
                 content: "hello yes"
             }
         });
-    } else {
-        response.status(200).end()
     }
+
+    response.end();
 }
