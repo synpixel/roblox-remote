61945dd29afddcea3e8fcfb74808941d7fcbb785
DogeyStudio <syntaxwastaken2@gmail.com>
2023-06-22 22:48:36 +0200

    try buttons

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,3 +1,13 @@
 export default async function () {
-  return { content: "Hello, world!", ephemeral: true };
+  return {
+    content: "Hello, world!",
+    components: [
+      {
+        type: 2,
+        style: 1,
+        label: "Authenticate",
+        url: "https://google.com/",
+      },
+    ],
+  };
 }
