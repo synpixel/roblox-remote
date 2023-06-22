11e150e6bce13b8b80de81ad65d286b05fee6083
DogeyStudio <syntaxwastaken2@gmail.com>
2023-06-22 23:24:59 +0200

    add debug message

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -6,6 +6,7 @@ export default async function (
   const authenticationSearchParams = new URLSearchParams();
 
   if (interaction.user) {
+    console.log("userExists");
     authenticationSearchParams.append("client_id", interaction.user.id);
     authenticationSearchParams.append("redirect_uri", "https://google.com/");
     authenticationSearchParams.append("scope", "openid profile");
