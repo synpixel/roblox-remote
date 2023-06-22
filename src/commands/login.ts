b96d00c4d9a58be50db42fa56a80a257fd5cb5e8
DogeyStudio <syntaxwastaken2@gmail.com>
2023-06-22 23:03:39 +0200

    fix button

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,5 +1,18 @@
 export default async function () {
   return {
     content: "Hello, world!",
+    components: [
+      {
+        type: 1,
+        components: [
+          {
+            type: 2,
+            style: 5,
+            label: "Authenticate",
+            url: "https://google.com/",
+          },
+        ],
+      },
+    ],
   };
 }
