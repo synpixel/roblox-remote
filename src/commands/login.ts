b1ecab6692aa2b7d7cca1a61cb86bef985cc04df
DogeyStudio <syntaxwastaken2@gmail.com>
2023-06-22 23:34:36 +0200

    accept CLIENT_ID environment variable

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,19 +1,17 @@
 import { APIBaseInteraction, InteractionType } from "discord-api-types/v10";
 
+const authenticationSearchParams = new URLSearchParams();
+
+authenticationSearchParams.append("client_id", process.env.CLIENT_ID as string);
+authenticationSearchParams.append("redirect_uri", "https://google.com/");
+authenticationSearchParams.append("scope", "openid profile");
+authenticationSearchParams.append("response_type", "code");
+
+const authenticationURL = `https://apis.roblox.com/oauth/v1/authorize?${authenticationSearchParams.toString()}`;
+
 export default async function (
   interaction: APIBaseInteraction<InteractionType.ApplicationCommand, undefined>
 ) {
-  const authenticationSearchParams = new URLSearchParams();
-
-  if (interaction.member) {
-    authenticationSearchParams.append("client_id", interaction.member.user.id);
-    authenticationSearchParams.append("redirect_uri", "https://google.com/");
-    authenticationSearchParams.append("scope", "openid profile");
-    authenticationSearchParams.append("response_type", "code");
-  }
-
-  const authenticationURL = `https://apis.roblox.com/oauth/v1/authorize?${authenticationSearchParams.toString()}`;
-
   return {
     content: "Log into your Roblox account by pressing the following button:",
     components: [
