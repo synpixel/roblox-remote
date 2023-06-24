fb34a56b6654d57ec5d6e4d81a271f04c2a92204
DogeyStudio <syntaxwastaken2@gmail.com>
2023-06-24 14:37:51 +0200

    fix db migrationg

--- migrate.ts
+++ migrate.ts
@@ -1,6 +1,9 @@
 import { sql } from "@vercel/postgres";
+import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { migrate } from "drizzle-orm/vercel-postgres/migrator";
+
+dotenv.config({ path: ".env.development.local" });
 
 const database = drizzle(sql);
 
