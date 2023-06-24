e757c308669975a73052d4b944d4e80e2a948ad2
DogeyStudio <syntaxwastaken2@gmail.com>
2023-06-24 16:11:09 +0200

    log options

--- api/interactions.ts
+++ api/interactions.ts
@@ -56,6 +56,7 @@ export default async function handler(
     > = request.body;
 
     console.log(interaction);
+    console.log(interaction.data.options);
 
     if (!interaction.data) {
       return {
